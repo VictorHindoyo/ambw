021c2342db62351d90efe84fb3c92db1d03f2829
VictorHindoyo <c14200126@john.petra.ac.id>
2023-03-31 08:19:37 +0700

    update sw.js

---
+++ app.js
@@ -0,0 +1,5 @@
+var title = document.querySelector('.title');
+var courseFeatureElements = document.querySelectorAll('.course-feature');
+var button = document.querySelector('button');
+
+navigator.serviceWorker.register('./sw.js');
