85ea0b2f6f54733736000604afded2ce6aada634
VictorHindoyo <c14200126@john.petra.ac.id>
2023-03-31 10:15:00 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
-navigator.serviceWorker.register('sw.js');
+var deferredPrompt;
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('./sw.js')
+    .then(function () {
+      console.log('Service worker registered!');
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
+}
 
 window.addEventListener('beforeinstallprompt', function(event) {
-    console.log('beforeinstallprompt fired');
-    event.preventDefault();
-    deferredPrompt = event;
-    return false;
-  });
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
